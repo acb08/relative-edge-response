fixed faulty transition pixel value calculation in chip_maker.py

src/chip_maker.py
@@ -16,19 +16,26 @@ def make_perfect_edge(size=rer_defs.pre_sample_size, theta=rer_defs.angle,
 
     edge_start = size / 2
     row_indices = np.arange(size)
-    edge_locations = edge_start + np.tan(theta / 360) * row_indices
+
+    edge_location_indices = np.arange(size + 1)
+    edge_locations = edge_start + np.tan(np.pi * theta / 180) * edge_location_indices
+
     edge_image = light_val * np.ones((size, size), dtype=np.float32)
     edge_vals = np.zeros(size)
+    edge_indices = np.zeros(size, dtype=np.int)
+    transition_pixel_fracs = np.zeros_like(edge_vals)
 
     for row_idx in row_indices:
-        edge_loc = int(edge_locations[row_idx])
-        transition_pixel_frac = edge_locations[row_idx] - edge_loc
+        edge_index = int(edge_locations[row_idx])
+        edge_indices[row_idx] = edge_index
+        transition_pixel_frac = 1 - (0.5 * (edge_locations[row_idx] + edge_locations[row_idx + 1]) - edge_index)
+        transition_pixel_fracs[row_idx] = transition_pixel_frac
         edge_val = transition_pixel_frac * (light_val - dark_val)
         edge_vals[row_idx] = edge_val
-        edge_image[row_idx, :edge_loc] = dark_val
-        edge_image[row_idx, edge_loc] = edge_val
+        edge_image[row_idx, :edge_index] = dark_val
+        edge_image[row_idx, edge_index] = edge_val
 
-    return edge_image, edge_locations, edge_vals
+    return edge_image, edge_indices, edge_locations, transition_pixel_fracs, edge_vals
 
 
 def get_blur_parameters(target_q, scale_factor, conversion=rer_defs.airy_gauss_conversion):